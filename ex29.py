7d28997b98820f924998e7bd409115b19fc0db2c
ChoChoThet <moetharkaungkincho@gmail.com>
2018-06-25 17:39:45 +0630

    if condition

--- ex29.py
+++ ex29.py
@@ -1,6 +1,6 @@
 people = 20
-cats =30
-dogs =15
+cats = 30
+dogs = 15
 if people < cats:
     print("Too many cats! The world is doomed!")
 if people > cats:
@@ -9,7 +9,7 @@ if people < dogs:
     print("The world is drooled on!")
 if people > dogs:
     print("The world is dry!")
-dogs +=5
+dogs += 5
 if people >= dogs:
     print("People are greater than or equal to dogs.")
 if people <= dogs:
