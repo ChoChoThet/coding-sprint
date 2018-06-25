ad6ba04fd1ed870d05226cb3c2400033dfbc79a5
ChoChoThet <moetharkaungkincho@gmail.com>
2018-06-25 17:48:24 +0630

    Print exercise with while loop

--- ex38.py
+++ ex38.py
@@ -1,13 +1,13 @@
 ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
-print("Wait there are not 10 things in that list. Let's fix that.")
+print(" Wait there are not 10 things in that list. Let's fix that.")
 
 stuff = ten_things.split(' ')
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
 
 while len(stuff) != 10:
     next_one = more_stuff.pop()
-    print("Adding: ", next_one)
+    print("Adding : ", next_one)
     stuff.append(next_one)
     print(f"There are {len(stuff)} items now.")
 
