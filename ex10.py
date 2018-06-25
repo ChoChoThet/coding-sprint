7b5d01f4623a0a421d7a7350fa5279775890e6d1
ChoChoThet <moetharkaungkincho@gmail.com>
2018-06-25 17:23:15 +0630

    Using escape character

--- ex10.py
+++ ex10.py
@@ -1,10 +1,10 @@
-tabby_cat="\tI'm tabbed in."
-persian_cat="I'm split\non a line."
-backslash_cat="I'm \\ a \\ cat."
-fat_cat="""
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+fat_cat = """
 I'll do a list:
     \t* Cat food 
-    \t* Fishies
+    \t* Fishes
     \t* Catnip\n\t* Grass
     """
 print(persian_cat)
