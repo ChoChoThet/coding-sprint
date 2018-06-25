3616dbc972135fddb2642b1a741c11ca86396997
ChoChoThet <moetharkaungkincho@gmail.com>
2018-06-25 17:51:24 +0630

    creating class

--- ex40.py
+++ ex40.py
@@ -8,12 +8,12 @@ class Song(object):
             print(line)
 
 
-happy_bday = Song(["Happy birthday to you",
-                   "I don't want to get sued", "So I'll stop right there"])
+happy_baby = Song([" Happy birthday to you",
+                   " I don't want to get sued", "So I'll stop right there"])
 
 bulls_on_parade = Song(["They rally around tha family",
                         "With pockets full of shells"])
 
-happy_bday.sing_me_a_song()
+happy_baby.sing_me_a_song()
 
 bulls_on_parade.sing_me_a_song()
